9bbfa01beb822c946b0ea8d85764c189712d9a09
vfuchs070114 <vincent.fuchs@sgcib.com>
2017-01-18 15:05:50 +0530

    moved to different package because of clash

--- src/main/java/org/springframework/cloud/contract/stubrunner/wrapper/StubRunnerServer.java
+++ src/main/java/org/springframework/cloud/contract/stubrunner/wrapper/StubRunnerServer.java
@@ -1,4 +1,4 @@
-package org.springframework.cloud.contract.stubrunner;
+package org.springframework.cloud.contract.stubrunner.wrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
